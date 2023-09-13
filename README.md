38a051160d6de674189e936877769ae00f83a5fe
Alphacode18 <skharban@purdue.edu>
2023-09-13 15:26:36 -0400

    adds prerequisites, download, and deployment section in the README.md

---
+++ README.md
@@ -0,0 +1,93 @@
+# Pronghorn Artifact Evaluation
+
+Pronghorn is a snapshot orchestrator for serverless platforms, which accelerates the average execution latency of deployed functions.
+
+## Getting Started
+
+### Prerequisite
+
+Hardware Requirements: We recommend employing a bare-metal machine with _A_ x86-64 CPUs, _B_ GB of memory, and _Z_ GB of storage.
+
+Software Requirements: 
+
+- Linux (Ubuntu 22.04 Recommended).
+- Docker Engine v20.10.12 or higher.
+- Kubernetes Server v1.21.1 or higher.
+
+Toolchain Requirements:
+
+- Multipass v1.12.2 or higher.
+
+```bash
+sudo snap install multipass
+```
+
+- Arkade v0.8.28 or higher.
+
+```bash
+curl -sLS https://get.arkade.dev | sudo sh
+```
+
+- Helm v3.5.2 or higher.
+
+```bash
+arkade get helm
+```
+
+- Kubectl v1.2.22 or higher.
+
+```bash
+arkade get kubectl
+```
+
+- k3sup v0.11.3 or higher.
+
+```bash
+arkade get k3sup
+```
+
+- faas-cli v0.14.2 or higher.
+
+```bash
+arkade get faas-cli
+```
+
+Configuration Requirements:
+
+- Enable Docker Buildkit Builder by adding the following to the `~/.docker/config.json` file and then restart the docker daemon.
+
+```bash
+{
+    ...
+    "experimental": true,
+    ...
+}
+```
+
+- Create the Docker Builder
+
+```bash
+docker buildx create --use --name pronghorn-builder --buildkitd-flags '--allow-insecure-entitlement security.insecure --allow-insecure-entitlement network.host'
+
+docker buildx inspect --bootstrap
+```
+
+### Download
+
+The code can be easily downloaded via:
+
+```bash
+git clone https://github.com/rssys/pronghorn-artifact.git
+```
+
+### Deployment
+
+⏰ Estimated time: _X_ machine minutes + _Y_ human minutes.
+
+The repository includes a `deploy.sh` script, which automates the process of spinning up and configuring a multi-node kubernetes cluster with our fork of OpenFaaS (an open-source serverless platform), MinIO (an open-source S3-compatible object store), and a lightweight key-value store.
+
+```bash
+chmod +x ./deploy.sh
+
+./deploy.sh
+```
---
+++ deploy.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+echo $DIR
+
+# Default configuration
+nodes=3
+cpus=2
+memory="8G"
+disk_size="50G"
+
+# Bootstrap The Nodes
+for node in $(seq 1 $nodes)
+do
+  if [ $node -eq 1 ];
+  then
+    echo "Deploying the master node"
+    multipass launch --cpus $cpus --memory $memory --disk $disk_size --name pronghorn 20.04 --cloud-init multipass.yaml
+    echo "Master node deployed"
+  else
+    echo Launching the worker nodes
+    multipass launch --cpus $cpus --memory $memory --disk $disk_size --name "pronghorn-m0$node" 20.04 --cloud-init multipass.yaml   
+    echo "Worker node $node deployed"
+  fi
+done
+
+# Deploy Kubernetes Cluster
+k3sup install --ip $(multipass info pronghorn | grep IPv4 | awk '{print $2}') --user ubuntu --k3s-extra-args '--cluster-init'
+for node in $(seq 2 $nodes)
+do
+    k3sup join --ip $(multipass info pronghorn-m0$node | grep IPv4 | awk '{print $2}') --server-ip $(multipass info pronghorn | grep IPv4 | awk '{print $2}') --user ubuntu
+done
+
+echo "[Completed] Kuberentes cluster created with $nodes nodes, $cpus CPUs, $memory MB memory and $disk_size disk size."
+
+# Set correct contexts
+export KUBECONFIG="$DIR/kubeconfig"
+echo "Remember to export KUBECONFIG=\"$DIR/kubeconfig\""
+
+# Install OpenFaaS
+arkade install openfaas --set faasnetes.image=skharban/faas-netes:privileged-containers &> /dev/null
+if kubectl get namespace openfaas &> /dev/null; then
+    echo "[Completed] OpenFaaS Installed."
+else
+    echo "[Error] OpenFaaS Installation Unsuccessful."
+    exit 1
+fi
+
+# Ensure gateway rollout is complete
+while true; do
+    pod_list=$(kubectl get pods -n openfaas | grep gateway)
+    if [ -n "$pod_list" ]; then
+        status=$(echo "$pod_list" | grep Running)
+        if [ -n "$status" ]; then
+            echo "[Completed] Gateway Rollout"
+            break;
+        else
+            echo "[Waiting] Gateway Rollout In Progress"
+            sleep 10
+        fi
+    else
+        echo "[Error] No Gateway Pod Found"
+        exit 1
+    fi
+done
+
+# Port forward OpenFaaS Gateway
+kubectl port-forward -n openfaas svc/gateway 8080:8080 & 
+
+# Export OpenFaaS Password
+export OPENFAAS_PASSWORD=$(kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo)
+echo $OPENFAAS_PASSWORD > .credentials
+
+# Authenticate OpenFaaS
+cat .credentials | faas-cli login --username admin --password-stdin
+
+# Deploy MinIO (Object Store)
+sed -e "s|DIR|$DIR|" $DIR/yaml/minio.yaml | kubectl apply -f -
+kubectl apply -f $DIR/yaml/minio-service.yaml
+
+# Deploy CRUD Service
+sed -e "s|DIR|$DIR|" $DIR/crud-service | kubectl apply -f -
+kubectl apply -f $DIR/crud-service/pod.yaml
+
+# Check MinIO deployment
+attempts=0
+pod_name=$(kubectl get pod -n store -o jsonpath='{.items[0].metadata.name}')
+
+while [[ "$attempts" -lt 3 ]]; do
+    pod_status=$(kubectl get pod -n store $pod_name -o jsonpath='{.status.phase}')
+    if [[ "$pod_status" == "Running" ]]; then
+        echo "[Completed] MinIO Deployed on Cluster."
+        break
+    else
+        echo "[Waiting] MinIO Deployment In Progress"
+        sleep 10
+        attempts=$((attempts+1))
+    fi
+done
+
+if [[ "$attempts" -eq 3 ]]; then
+    echo "[Error] MinIO Deployment Failed."
+    exit 1
+fi
+
+# Check if MinIO service is running
+attempts=0
+
+while [[ "$attempts" -lt 3 ]]; do
+    service_ip=$(kubectl get svc minio-svc -n store -o jsonpath='{.spec.clusterIP}')
+    if [[ -n "$service_ip" ]]; then
+        echo "[Completed] MinIO Service Deployed."
+        break
+    else
+        echo "[Waiting] MinIO Service Deployment In Progress"
+        sleep 10
+        attempts=$((attempts+1))
+    fi
+done
+
+if [[ "$attempts" -eq 3 ]]; then
+    echo "[Error] MinIO Service Deployment Failed."
+    exit 1
+fi
