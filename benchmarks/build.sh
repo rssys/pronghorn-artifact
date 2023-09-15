5a2b3f9596d3e2cbd1aa87bfdf636cc3c75fc247
Alphacode18 <skharban@purdue.edu>
2023-09-14 20:04:16 -0400

    minor fixes

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
+!/bin/bash
 
-# List of all functions
-# pypy_functions=( bfs dfs dynamic-html mst pagerank compress upload thumbnail video )
-# jvm_functions=( matrix-multiplication simple-hash word-count html-rendering )
+List of all functions
+pypy_functions=( bfs dfs dynamic-html mst pagerank compress upload thumbnail video )
+jvm_functions=( matrix-multiplication simple-hash word-count html-rendering )
 
-# joined_pypy=$(IFS=','; echo "${pypy_functions[*]}")
-# joined_jvm=$(IFS=','; echo "${jvm_functions[*]}")
-
-# bash build_runtime.sh $joined_pypy pypy
-# bash build_runtime.sh $joined_jvm jvm
-
-pypy_functions=( bfs )
 joined_pypy=$(IFS=','; echo "${pypy_functions[*]}")
+joined_jvm=$(IFS=','; echo "${jvm_functions[*]}")
+
 bash build_runtime.sh $joined_pypy pypy
+bash build_runtime.sh $joined_jvm jvm
