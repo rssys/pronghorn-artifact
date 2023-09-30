623b204c9d540245752069756fef80f25ef82a23
Shreyas Kharbanda <skharban@purdue.edu>
2023-09-30 10:25:27 -0400

    Update build.sh

--- benchmarks/build.sh
+++ benchmarks/build.sh
@@ -1,11 +1,11 @@
 !/bin/bash
 
 #List of all functions
-# pypy_functions=( bfs dfs dynamic-html mst pagerank compress upload thumbnail video )
+pypy_functions=( bfs dfs dynamic-html mst pagerank compress upload thumbnail video )
 jvm_functions=( matrix-multiplication simple-hash word-count html-rendering )
 
-# joined_pypy=$(IFS=','; echo "${pypy_functions[*]}")
+joined_pypy=$(IFS=','; echo "${pypy_functions[*]}")
 joined_jvm=$(IFS=','; echo "${jvm_functions[*]}")
 
-# bash build_runtime.sh $joined_pypy pypy
+bash build_runtime.sh $joined_pypy pypy
 bash build_runtime.sh $joined_jvm jvm
